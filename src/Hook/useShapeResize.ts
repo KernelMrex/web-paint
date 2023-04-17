3f2633c6269a3d815204c122443b43df50a63af8
KernelMrex <24671528+KernelMrex@users.noreply.github.com>
2023-04-17 16:56:45 +0300

    Добавил исправление бага с отрицательным ресайзом

--- src/Hook/useShapeResize.ts
+++ src/Hook/useShapeResize.ts
@@ -8,9 +8,11 @@ function useShapeResize<T extends SVGElement>(
     setShapeFrame: (width: number, height: number) => void,
 ) {
     let prevPos = { x: 0, y: 0 };
+    let shapeDelta = { width: shape.Frame().width, height: shape.Frame().height };
 
     const onMoveStart = (e: MouseEvent) => {
         prevPos = { x: e.pageX, y: e.pageY };
+        shapeDelta = { width: shape.Frame().width, height: shape.Frame().height }
     }
 
     const onMove = (e: MouseEvent) => {
@@ -19,9 +21,14 @@ function useShapeResize<T extends SVGElement>(
             height: e.pageY - prevPos.y,
         }
 
+        shapeDelta = {
+            width: shapeDelta.width + delta.width,
+            height: shapeDelta.height + delta.height,
+        }
+
         setShapeFrame(
-            shape.Frame().width + delta.width,
-            shape.Frame().height + delta.height,
+            (shapeDelta.width > 1) ? (shape.Frame().width + delta.width) : 1,
+            (shapeDelta.height > 1) ? (shape.Frame().height + delta.height) : 1,
         );
 
         prevPos = {
